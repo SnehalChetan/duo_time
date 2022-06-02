5008708323272b28100c6cecd9f16e829f1c72e4
Greet Verduyn <greetverduyn@proximus.be>
2022-06-02 12:47:41 +0200

    changed colorbox en replaced it + changed picture of celebrity

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 make assigned partner's portfolio without getting info from them
 
 https://snehalchetan.github.io/duo_time/
+
+[]flip dot
+[] change color dot
+[] change color random !!! put random in the function!
+[] make timer
+[] make countr to stop flipping
--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-/*
-* javascript for duo-time
-*/
+let chaser = document.getElementsByClassName("chaser")[0]
+console.log(chaser);
+let counter = 0;
+chaser.style.position = 'absolute';
+chaser.style.left = "50px";
+chaser.style.top = "250px";
+
+function flip() {
+    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    chaser.style.transform = 'rotate(90deg)';
+    chaser.style.backgroundColor = randomColor;
+    counter ++;
+    chaser.style.left= parseInt(chaser.style.left) +50 +"px";
+    if (counter < 10) {
+        setTimeout(flip, 1000);
+    }
+};
+
+chaser.addEventListener("click", function () {
+    counter=0
+    flip()
+});
+
+document.getElementsByClassName("colorBox")[0].style.backgroundColor= 'coral';
+document.getElementsByClassName("lead")[0].style.height='50px';
+let images= document.getElementsByTagName("img")[4].src='./img/Nicole_Kidman.jpg'
+console.log(images);
