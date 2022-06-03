9181bcded24226f66ac067d2cd793321b23d8d0e
Greet Verduyn <greetverduyn@proximus.be>
2022-06-03 10:02:17 +0200

    change picture movie

--- js/script.js
+++ js/script.js
@@ -23,35 +23,49 @@ chaser.addEventListener("click", function () {
 
 let colorBox=document.getElementsByClassName("colorBox");
 
-colorBox[0].style.backgroundColor= 'coral';
-colorBox[0].style.height= "412px";
+colorBox[0].style.backgroundColor= 'salmon';
+colorBox[0].style.height= "500px";
 colorBox[0].style.width= "350px";
-document.getElementsByClassName("lead")[0].style.height='50px';
+colorBox[0].style.borderBottomLeftRadius= "25px";
+colorBox[0].style.borderBottomRightRadius= "25px";
+
+let lead=document.getElementsByClassName("lead");
+    lead[0].style.height='50px';
+    lead[1]
+console.log(lead)
+
 let bigText= document.getElementsByClassName("display-5")
     bigText[2].style.height='110px';
     bigText[3].style.height='110px';
 
 let images = document.getElementsByTagName("img");
 images[4].src='./img/Nicole_Kidman.jpg';
-images[4].style.width= "350px";
-images[4].style.height= "412px";
+images[4].style.width='350px';
+images[4].style.height='500px';
 images[1].src='./img/kitten.jpg';
-images[1].style.width= "350px";
-images[2].style.height= "412px";
-images[2].style.width= "350px";
-images[3].style.height= "412px";
-images[3].style.width= "350px";
-images[5].style.height= "412px";
-images[5].style.width= "350px";
+images[1].style.width='350px';
+images[1].style.height='500px';
+images[2].style.height='500px';
+images[2].style.width= '350px';
+images[3].style.height='500px';
+images[3].style.width='350px';
+images[5].src='./img/theOthers.jpg';
+images[5].style.height='500px';
+images[5].style.width='350px';
+images[0].style.borderRadius='25px'
 
 let shadowBox=document.getElementsByClassName("shadowBox");
+shadowBox[0].style.height ='500px';
 shadowBox[1].style.height ='500px';
-shadowBox[2].style.display='500px';
-shadowBox[3].style.display='500px';
-shadowBox[4].style.display='500px';
-shadowBox[5].style.display='500px';
+shadowBox[2].style.height='500px';
+shadowBox[3].style.heigth='500px';
+shadowBox[4].style.height='500px';
+shadowBox[5].style.height='500px';
+document.getElementsByClassName("greetImg shadow-sm d-none d-md-block")[0].style.backgroundColor='# 248,249,250';
 
-let bigBox=document.getElementsByClassName("me-md-3");
-
-console.log('shadowBox',shadowBox);
+/*let bigBox=document.getElementsByClassName("me-md-3");
+bigBox[0].style.display='900px';
+bigBox[1].style.display='900px';
+bigBox[2].style.display='900px';
+console.log('bigBox',bigBox);*/
 //console.log(images);
